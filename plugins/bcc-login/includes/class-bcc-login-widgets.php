a30fac151df194603ea48e53c34734c874d3bf13
Simon Costea <simoncostea@yahoo.com>
2022-09-15 19:03:05 +0300

    Add BCC widgets
    - temporary solution to the access token since the current solution does not work
    - make the widgets.css always load since it is used by the widgets

--- plugins/bcc-login/includes/class-bcc-login-widgets.php
+++ plugins/bcc-login/includes/class-bcc-login-widgets.php
@@ -12,18 +12,17 @@ class BCC_Login_Widgets {
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_styles' ) );
         add_action( 'wp_head', array( $this, 'render_topbar' ) );
         add_filter( 'body_class', array( $this, 'body_class' ) );
+        add_action( 'init', array( $this, 'bcc_widgets' ) );
     }
 
     function enqueue_styles() {
-        if ( $this->should_show_topbar() ) {
-            wp_enqueue_style( 'bcc-login-widgets', 'https://widgets.bcc.no/styles/widgets.css' );
-            wp_add_inline_style( 'bcc-login-widgets', '@media screen and (max-width: 600px){.admin-bar .portal-top-bar{position:absolute;}}@media screen and (min-width: 850px){body{margin-top:48px!important;}.portal-top-bar-spacer{display:none;}.admin-bar .portal-top-bar{top:46px;}}' );
-        }
+        wp_enqueue_style( 'bcc-login-widgets', 'https://widgets.bcc.no/styles/widgets.css' );
+        wp_add_inline_style( 'bcc-login-widgets', '@media screen and (max-width: 600px){.admin-bar .portal-top-bar{position:absolute;}}@media screen and (min-width: 850px){body{margin-top:48px!important;}.portal-top-bar-spacer{display:none;}.admin-bar .portal-top-bar{top:46px;}}' );
     }
 
     function render_topbar() {
         if ( $this->should_show_topbar() ) {
-            echo '<script id="script-bcc-topbar" data-authentication-type="WebApp" data-authentication-location="'. site_url( 'bcc-login/access-token/' ) . '" src="https://widgets.bcc.no/widgets/TopbarJs" defer></script>' . PHP_EOL;
+            echo '<script id="script-bcc-topbar" data-authentication-type="inline-access-token" data-access-token="' . $this->get_access_token() . '" src="https://widgets.bcc.no/widgets/TopbarJs" defer></script>' . PHP_EOL;
         }
     }
 
@@ -41,5 +40,53 @@ class BCC_Login_Widgets {
             ! wp_is_json_request() &&
             ! is_customize_preview()
         );
+    }
+
+    function bcc_widgets() {
+        add_shortcode( 'bcc-widgets-week-calendar', function ($attributes) {
+            $attributes = array_change_key_case((array)$attributes, CASE_LOWER);
+
+            $html =  '<div id="bcc-calendar-week"></div>';
+            $html .= '<script id="script-bcc-calendar-week" data-authentication-type="inline-access-token" data-access-token="' . $this->get_access_token() . '" ';
+            $html .= 'data-language="' . $attributes['language'] . '" data-maxdays="' .  $attributes['maxdays'] . '" data-maxappointments="' . $attributes['maxappointments'] . '" ';
+            $html .= 'data-calendars="' . $attributes['calendars'] .'" data-fullcalendarurl="' .  $attributes['fullcalendarurl'] . '" ';
+            $html .= 'src="https://widgets.bcc.no/widgets/CalendarWeekJs" defer></script>';
+
+            return $html . PHP_EOL;
+        } );
+
+        add_shortcode( 'bcc-widgets-month-calendar', function ($attributes) {
+            $attributes = array_change_key_case((array)$attributes, CASE_LOWER);
+
+            $html =  '<div id="bcc-calendar-month"></div>';
+            $html .= '<script id="script-bcc-calendar-month" data-authentication-type="inline-access-token" data-access-token="' . $this->get_access_token() . '" ';
+            $html .= 'data-language="' . $attributes['language'] . '" data-calendars="' . $attributes['calendars'] . '" ';
+            $html .= 'src="https://widgets.bcc.no/widgets/CalendarMonthJs" defer></script>';
+            
+            return $html . PHP_EOL;
+        } );
+
+        add_shortcode( 'bcc-widgets-birthday', function ($attributes) {
+            $attributes = array_change_key_case((array)$attributes, CASE_LOWER);
+
+            $html = '<div id="bcc-birthday"></div>';
+            $html .= '<script id="script-bcc-birthday" data-authentication-type="inline-access-token" data-access-token="' . $this->get_access_token() . '" ';
+            $html .= 'data-language="' . $attributes['language'] . '" data-churchname="' . $attributes['churchname'] . '" data-maxdays="' . $attributes['maxdays'] . '" ';
+            $html .= 'src="https://widgets.bcc.no/widgets/BirthdayJs" defer></script>';
+
+            return $html . PHP_EOL;
+        } );
+    }
+
+    function get_access_token() {
+        $token = $_SESSION['oidc_access_token'];
+
+        if ( empty( $token ) ) {
+            $token_id = $_COOKIE['oidc_token_id'];
+            $token = get_transient( 'oidc_access_token_' . $token_id );
+            $_SESSION['oidc_access_token'] = $token;
+        }
+        
+        return $token;
     }
 }
