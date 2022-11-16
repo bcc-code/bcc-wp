e8980e727574d338e1c72ac3c6eb304e591e00e0
Reng van Oord <rvanoord@users.noreply.github.com>
2022-11-16 12:20:01 +0100

    Added support for new OIDC backchannel logout (#59)
    * Added support for new OIDC backchannel logout
    
    * Removed redundant code

--- plugins/bcc-login/endpoints/logout.php
+++ plugins/bcc-login/endpoints/logout.php
@@ -1,20 +1,42 @@
 <?php
 
+require_once( 'includes/class-bcc-login-token-utility.php');
+
 /** backchannel logout */
-$state = $_GET['state'];
-if ( ! $state || empty ( $state ) ) {
-    $state = $_POST['state'];
-}
+$logout_token = $_POST['logout_token'];
+$token_id = '';
 
-if ( ! empty( $state ) ) {
-    $token_id = get_transient( 'oidc_token_id_' . $state );
+if ( $logout_token )
+{
+    // OIDC backchannel logout. Retrieve session ID from logout_token.
+    $logout_token_claims = BCC_Login_Token_Utility::get_token_claims( $logout_token );
+    $sid = $logout_token_claims['sid'];
+    if ( $sid && ! empty ( $sid )) {
+        $token_id = md5 ( $sid );
+    }
+    if ( ! empty( $token_id ) ) {
+        $state = get_transient( 'oidc_state_' . $token_id );
+    }
 
-    if ( $token_id ) {
-        delete_transient( 'oidc_state_' . $token_id );
-        delete_transient( 'oidc_access_token_' . $token_id );
-        delete_transient( 'oidc_id_token_' . $token_id ); 
-        delete_transient( 'oidc_token_id_' . $state );
+} else {
+    // "Old" backchannel logout, based on state parameter
+    $state = $_GET['state'];
+    if ( ! $state ) {
+        $state = $_POST['state'];
+    }
+
+    if ( ! empty( $state ) ) {
+        $token_id = get_transient( 'oidc_token_id_' . $state );
     }
 }
+
+// Clear login session transients
+if ( $token_id ) {
+    delete_transient( 'oidc_state_' . $token_id );
+    delete_transient( 'oidc_access_token_' . $token_id );
+    delete_transient( 'oidc_id_token_' . $token_id ); 
+    delete_transient( 'oidc_token_id_' . $state );
+}
+
 
 ?>
---
+++ plugins/bcc-login/includes/class-bcc-login-token-utility.php
@@ -0,0 +1,32 @@
+
+<?php
+
+class BCC_Login_Token_Utility {
+
+    public static function get_token_claims( $id_token ) {
+        // Check if id token exists
+        if ( ! isset( $id_token ) ) {
+            return array();
+        }
+
+        // Break apart the id_token in the response for decoding.
+        $tmp = explode( '.', $id_token );
+
+        if ( ! isset( $tmp[ 1 ] ) ) {
+            return array();
+        }
+
+        // Extract the id_token's claims from the token.
+        return json_decode(
+            base64_decode(
+                str_replace( // Because token is encoded in base64 URL (and not just base64).
+                    array( '-', '_' ),
+                    array( '+', '/' ),
+                    $tmp[ 1 ]
+                )
+            ),
+            true
+        );
+    }
+
+}
